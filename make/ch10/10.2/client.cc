1d977ece4b315c06c393e97e0e4e7b2aa9bb7b21
Issac <changfeng.or.chen79@gmail.com>
2017-08-01 15:12:32 +0800

    10.2 Catalogue of Build-In Rules

---
+++ make/ch10/10.2/client.cc
@@ -0,0 +1,9 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char **argv)
+{
+	cout << "Hello Compiling C++ programs!" << endl;
+	return 0;
+}
---
+++ make/ch10/10.2/main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+	printf("10.2 Cataglogue of built-In Rules\n");
+	return 0;
+}
